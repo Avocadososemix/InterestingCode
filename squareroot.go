547d7041f6e14f5b560af34c7d3dfcce5827dff1
Avocadososemix <lauri.karanko@gmail.com>
2018-10-23 12:31:36 +0300

    added name of the method used

--- squareroot.go
+++ squareroot.go
@@ -1,4 +1,5 @@
 // golang squareroot calculation, z in the loop can be changed, as well as the amount of loop repetitions to change accuracy
+// This method is called Newton's method, more about it can be found on wikipeida https://en.wikipedia.org/wiki/Newton%27s_method
 
 package main
 
